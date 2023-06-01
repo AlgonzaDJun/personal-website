d440e0282bb1006a22afb349ae8ff0c6bca526a0
AlgonzaDJun <algonsadewangga@gmail.com>
2023-06-01 14:20:00 +0700

    hover

--- src/app/about/About.jsx
+++ src/app/about/About.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "../css/about.css";
 
 export const About = () => {
+  const [cardDesc, setCardDesc] = useState(false)
+
   const isiCard = [
     {
       judul: "Siapa Saya ?",
@@ -29,7 +32,26 @@ export const About = () => {
         </div>
       </div>
       <div className="w-100 d-flex flex-wrap justify-content-around align-items-center">
-        {isiCard.map((item, index) => {
+        <div className="mt-5">
+          <div
+            className="card border-0"
+          >
+            <div
+              className="card-2 h-100 w-100 d-flex flex-column"
+              style={{ backgroundImage: `url(${isiCard[0].image})` }}
+            >
+              <h4 className="judul-card fw-bold fs-3 mt-auto">
+                {isiCard[0].judul}
+              </h4>
+            </div>
+            <div className="ps-4 card-desc">
+              <h5 id="about" className="lh-base">
+                {isiCard[0].deskripsi}
+              </h5>
+            </div>
+          </div>
+        </div>
+        {/* {isiCard.map((item, index) => {
           return (
             <div key={index} className="p-4 mt-5">
               <div className="card border-0">
@@ -47,7 +69,7 @@ export const About = () => {
               </div>
             </div>
           );
-        })}
+        })} */}
       </div>
     </div>
   );
