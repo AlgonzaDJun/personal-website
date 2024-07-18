db2fdea10f07ba39965813258c48e85f4b907c13
AlgonzaDJun <algonsadewangga@gmail.com>
2024-07-18 13:57:00 +0700

    change connect db

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -2,14 +2,7 @@
 
  const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_LOCAL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-
-            // wait for upload more than 10s
-            serverSelectionTimeoutMS: 10000,
-            socketTimeoutMS: 45000,
-        })
+        await mongoose.connect(process.env.MONGO_LOCAL)
         console.log('MongoDB is connected')
     } catch (error) {
         console.log(error)
